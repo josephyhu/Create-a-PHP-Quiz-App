335b098f34d0cdda5aaa12a6eca7e1753047e55a
Joseph Yhu <xbhlx7@gmail.com>
2019-08-03 23:46:24 -0400

    add files

---
+++ inc/quiz.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * PHP Techdegree Project 2: Build a Quiz App in PHP
+ *
+ * These comments are to help you get started.
+ * You may split the file and move the comments around as needed.
+ *
+ * You will find examples of formating in the index.php script.
+ * Make sure you update the index file to use this PHP script, and persist the users answers.
+ *
+ * For the questions, you may use:
+ *  1. PHP array of questions
+ *  2. json formated questions
+ *  3. auto generate questions
+ *
+ */
+
+// Include questions
+
+// Keep track of which questions have been asked
+
+// Show which question they are on
+// Show random question
+// Shuffle answer buttons
+
+
+// Toast correct and incorrect answers
+// Keep track of answers
+// If all questions have been asked, give option to show score
+// else give option to move to next question
+
+
+// Show score
