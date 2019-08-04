7e56dc89b04c75941edcdf4af146d7d205a6ca36
Joseph Yhu <xbhlx7@gmail.com>
2019-08-04 03:24:06 -0400

    Update

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,13 +1,26 @@
 <?php
 // Generate random questions
-
+$questions = [];
 // Loop for required number of questions
-
+for ($i = 0; $i <= 9; $i++) {
 // Get random numbers to add
-
+    $a = rand(0, 100);
+    $b = rand(0, 100);
 // Calculate correct answer
-
+    $answer = $a + $b;
 // Get incorrect answers within 10 numbers either way of correct answer
 // Make sure it is a unique answer
-
+    if ($answer >= 10) {
+      $wrongAnswer1 = $answer - rand(1, 10);
+      $wrongAnswer2 = $answer + rand(1, 10);
+    } else {
+      $wrongAnswer1 = $answer + rand(1, 10);
+      $wrongAnswer2 = $answer + rand(1, 10);
+    }
 // Add question and answer to questions array
+    $questions[$i]["leftAdder"] = $a;
+    $questions[$i]["rightAdder"] = $b;
+    $questions[$i]["correctAnswer"] = $answer;
+    $questions[$i]["firstIncorrectAnswer"] = $wrongAnswer1;
+    $questions[$i]["secondIncorrectAnswer"] = $wrongAnswer2;
+}
--- inc/quiz.php
+++ inc/quiz.php
@@ -16,13 +16,26 @@
  */
 
 // Include questions
-
+include 'generate_questions.php';
 // Keep track of which questions have been asked
 
 // Show which question they are on
 // Show random question
 // Shuffle answer buttons
-
+function quiz() {
+    global $questions;
+    $array = ["correctAnswer", "firstIncorrectAnswer", "secondIncorrectAnswer"];
+    shuffle($array);
+    $random = rand(0, 9);
+    echo "<p class='breadcrumbs'>" . "Question # of 10" . "</p>";
+    echo "<p class='quiz'>" . "What is " . $questions[$random]["leftAdder"] . " + " . $questions[$random]["rightAdder"] . "?" . "</p>";
+    echo "<form action='index.php' method='post'>";
+    echo "<input type='hidden' name='id' value='0' />";
+    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$random][$array[0]] . "'>";
+    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$random][$array[1]] . "'>";
+    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$random][$array[2]] . "'>";
+    echo "</form>";
+  }
 
 // Toast correct and incorrect answers
 // Keep track of answers
