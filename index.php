30d2244bb51adb8293c177ed746596dae7230976
Joseph Yhu <xbhlx7@gmail.com>
2019-10-07 20:33:54 -0400

    Update

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php include 'inc/header.php'; ?>
     <div class="container">
       <div id="quiz-box">
-        <a href="addition.php">Addition</a>
-        <a href="subtraction.php">Subtraction</a>
-        <a href="multiplication.php">Multiplication</a>
-        <a href="division.php">Division</a>
-        <a href="all.php">All</a>
+        <nav>
+          <a href="addition.php">Addition</a>
+          <a href="subtraction.php">Subtraction</a>
+          <a href="multiplication.php">Multiplication</a>
+          <a href="division.php">Division</a>
+          <a href="all.php">All</a>
+        </nav>
       </div>
     </div>
 <?php include 'inc/footer.php'; ?>
